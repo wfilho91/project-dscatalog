fd4e109e96cb2e53cba9e5b16ad8dac8a22f443c
wfilho91 <wellington79620@gmail.com>
2022-11-22 16:17:04 -0300

    Category resource

---
+++ server/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Transactional(readOnly = true)
+	public List<Category> findAll(){
+		List<Category> categories = categoryRepository.findAll();
+		return categories;
+	}
+}
